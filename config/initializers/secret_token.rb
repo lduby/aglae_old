803583dc337d1ed259f61cb0848baa7f44219247
Laetitia Duby <laetitia.duby@gmail.com>
2012-07-12 14:40:22 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aglae::Application.config.secret_token = 'b2a9f75a078c5be6421ed5764670ebcbe5e490f3682553c9a669eb03c9815d8e7b62b227af25144c1eb7f31f6aa003d6a7644ea246ce4b2562d6b93bad861478'
